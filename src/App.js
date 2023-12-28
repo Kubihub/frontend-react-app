d79ada9b8f81d1e27584e2652f74e1b081fbb2d9
Kubihub <kuburatuadiza5@gmail.com>
2023-12-28 22:07:50 +0000

    New project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from './components/Header';
+import Services from './components/Services';
+import Slider from './components/Slider';
+import Article from './components/Article';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <Services />
+    <Slider />
+    <Article />
+    <Footer />
+      
+    </div>
+  )
+}
+
+export default App;
